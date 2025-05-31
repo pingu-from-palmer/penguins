1a475933b5cc3096e3b4f2665237c372d2191d29
Pingu <pingu.from.palmer@gmail.com>
2025-05-31 17:46:49 +0000

    Corrected the annotation for the scatter plot

--- my_code.R
+++ my_code.R
@@ -1,3 +1,9 @@
-# This is my first line of code
-ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()+labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species") +
-  theme_minimal()
+install.packages("palmerpenguins")
+install.packages("ggplot2")
+
+library(ggplot2)
+library(palmerpenguins)
+
+# The code below creates a scatter plot for the correlation between flipper length and body mass of different penguin species.
+
+ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()+labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")
