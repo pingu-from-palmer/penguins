829256a86afbc6716796c67944527abf29d6291a
Pingu <pingu.from.palmer@gmail.com>
2025-05-31 18:11:43 +0000

    Change to my_code.R: Added the regression line to the scatter plot

--- my_code.R
+++ my_code.R
@@ -6,4 +6,4 @@ library(palmerpenguins)
 
 # The code below creates a scatter plot for the correlation between flipper length and body mass of different penguin species.
 
-ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()+labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")
+ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()+labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")+ geom_smooth(method = "lm", se = FALSE) + # Add a regression line to the scatter plot
