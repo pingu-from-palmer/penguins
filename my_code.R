090d6f93a24c8b4930c709c5ce2b3d59a5998f1f
Pingu <pingu.from.palmer@gmail.com>
2025-05-31 18:13:29 +0000

    Change to my_code.R: Added a theme to the scatted plot

--- my_code.R
+++ my_code.R
@@ -7,3 +7,4 @@ library(palmerpenguins)
 # The code below creates a scatter plot for the correlation between flipper length and body mass of different penguin species.
 
 ggplot(data = penguins, aes(x = flipper_length_mm, y = body_mass_g, color = species)) + geom_point()+labs(x = "Flipper Length (mm)", y = "Body Mass (g)", color = "Species")+ geom_smooth(method = "lm", se = FALSE) + # Add a regression line to the scatter plot
++ theme_minimal() # Add a theme to our plot
